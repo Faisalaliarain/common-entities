b6ffa558edb9432d063cfb4dd7cc99a823759046
faisalaliarain <faisalali.ali7@gmail.com>
2022-12-08 18:37:56 +0500

    updates
    update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 import { User } from "./entities/user.entity";
+import { UserToken } from "./entities/user_token.entity";
+import { SupportedAddresses } from "./entities/supported_addresses.entity";
+import { SupportedTokens } from "./entities/supported_tokens.entity";
 
-export { User };
+export { User, UserToken, SupportedAddresses, SupportedTokens };
