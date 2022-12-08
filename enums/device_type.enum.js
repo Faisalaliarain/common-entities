3a59e0d2eabb803ea07e3550be445e3a88438048
faisalaliarain <faisalali.ali7@gmail.com>
2022-12-08 14:37:46 +0500

    changes

--- enums/device_type.enum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.DEVICE_TYPE = void 0;
-var DEVICE_TYPE;
-(function (DEVICE_TYPE) {
-    DEVICE_TYPE["ANDROID"] = "ANDROID";
-    DEVICE_TYPE["IOS"] = "IOS";
-    DEVICE_TYPE["WINDOWS"] = "WINDOWS";
-})(DEVICE_TYPE = exports.DEVICE_TYPE || (exports.DEVICE_TYPE = {}));
